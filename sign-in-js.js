eb99bdcd37c765fcc9fcd663f9d40b7d7070d8c2
ni-shat <nishat.tabassum301@gmail.com>
2023-03-05 23:21:24 +0600

    added few things

---
+++ sign-in-js.js
@@ -0,0 +1,7 @@
+
+
+let menu = document.getElementById("menu");
+const showMenu = (flag) => {
+    menu.classList.toggle("hidden");
+    // document.getElementById('menu').style.display = 'none';
+};
